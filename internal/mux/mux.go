79c36c03016fa02ec68307ee17b0f70b62594064
teka808 <teka.valiev@gmail.com>
2022-10-01 16:38:16 +0900

    bug fixed

--- internal/mux/mux.go
+++ internal/mux/mux.go
@@ -25,6 +25,6 @@ func Init() {
 func (r *mRouter) setHandlers() {
 	r.router.HandleFunc("/devices/{id}", handlers.GetDeviceByIdHandler).Methods("GET")
 	r.router.HandleFunc("/devices/type/{type}", handlers.GetDeviceByTypeHandler).Methods("GET")
-	r.router.HandleFunc("/devices/status/{type}", handlers.GetDeviceByStatusHandler).Methods("GET")
+	r.router.HandleFunc("/devices/status/{status}", handlers.GetDeviceByStatusHandler).Methods("GET")
 	r.router.HandleFunc("/devices", handlers.GetDevices).Methods("GET")
 }
